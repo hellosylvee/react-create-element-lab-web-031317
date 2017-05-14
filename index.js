c86ff79fe1809a8c27a0ec5d7ec9c1c1816bf0aa
hellosylvee <helloimsylvia@gmail.com>
2017-05-13 20:12:21 -0400

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const listItem2 = React.createElement('li', {}, 'React');
 const listItem3 = React.createElement('li', {}, 'Movies');
 const listItem4 = React.createElement('li', {}, 'Ice cream');
 
-const unorderedList = React.createElement('ul', { className: 'me__interests'}, [ listItem1, listItem2, listItem3, listItem4 ]);
-
-
+const unorderedList = React.createElement('ul', { className: 'me__interests' }, [ listItem1, listItem2, listItem3, listItem4 ]);
 const meInReact = React.createElement('div', { className: 'me' }, [ header1, paragraph, unorderedList]);
 
 ReactDOM.render(
